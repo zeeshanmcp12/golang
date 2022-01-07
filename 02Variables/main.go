4e175dd189e50d9520e844300741929d5aba9e97
zeeshanmcp12 <zeeshanmcp12@gmail.com>
2022-01-07 20:45:57 +0500

    golang types and const complete

--- 02Variables/main.go
+++ 02Variables/main.go
@@ -2,6 +2,8 @@ package main
 
 import "fmt"
 
+const LoginName string = "zeeshanmcp12" // Public, so it is available across the files of program.
+
 func main() {
 	// fmt.Println("Variables!")
 
@@ -9,6 +11,7 @@ func main() {
 	fmt.Println(username)
 	fmt.Printf("Type of var username is %T \n", username)
 	fmt.Printf("----------------------------------------- \n")
+	// This % sign(before letter 'T') is a placeholder.
 
 	var isRegistered bool = true
 	fmt.Println("Did you Signup today?", isRegistered)
@@ -25,13 +28,38 @@ func main() {
 	fmt.Printf("Type of var smallFloat is %T \n", smallFloat)
 	fmt.Printf("----------------------------------------- \n")
 
+	// Default value and some aliases
+	var intVariable int
+	fmt.Println(intVariable)
+	// This variable is just declared and not initialized and golang will not add garbage value into it but print 0 because it is integer as we described it's type.
+	fmt.Printf("Type of var intVariable is %T \n", intVariable)
+	fmt.Printf("----------------------------------------- \n")
+
+	var stringVariable string
+	fmt.Println(stringVariable)
+	// This variable is just declared and not initialized. golang will not add garbage value into it but print nothing if the variable is of type string.
+	fmt.Printf("Type of var stringVariable is %T \n", stringVariable)
+	fmt.Printf("----------------------------------------- \n")
+
+	// Implicit type or an implicit way of declaring variables
 	fmt.Printf("Variable declared without mentioning it's type.\n")
 	var fullName = "Muhammad Zeeshan"
 	fmt.Println(fullName)
 	fmt.Printf("Type of var fullName is %T \n", fullName)
 	fmt.Printf("----------------------------------------- \n")
+	// In this case lexer will identify whether it is of type string or else. But, we cannot reinitialize this variable with different type i.e. int or float etc. It will give error saying "already declared this varibale to string"
+	// fullName = 3 -> this is not allowed.
 
-	fmt.Printf("Implicit variable declaration.\n")
+	// No var style of declaring variable.
 	text := "This is text"
 	fmt.Println(text)
+	fmt.Printf("No var style of declaring variable. \n")
+	fmt.Printf("----------------------------------------- \n")
+	// No var keyword here. We just used ":="
+	// Note: Inside of any method we can use this volorous operator (:=) to declare a variable but outside of method it is not allowed. for example global declaration or outside of main function.
+
+	// for const declaration, please go to top.
+
+	fmt.Println(LoginName)
+	fmt.Printf("The type of LoginName variable is %T \n", LoginName)
 }
