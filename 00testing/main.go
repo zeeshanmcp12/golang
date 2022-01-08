582ea1dc66f3163d9fe4bcbe7281f6462ac5cf38
zeeshanmcp12 <zeeshanmcp12@gmail.com>
2022-01-09 00:07:57 +0500

    00testing added

---
+++ 00testing/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var username string = "zeeshanmcp12"
+	fmt.Println(username)
+	fmt.Printf("Type of var username is %T \n", username)
+
+	fullName := "Muhammad Zeeshan"
+	fmt.Println("My name is ", fullName)
+
+	var isLoggedin = true
+	fmt.Printf("Type of var isLoggedin %T \n", isLoggedin)
+
+}
