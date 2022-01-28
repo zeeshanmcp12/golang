62d0b253ede8136b3db46fc8294dfc2c4c00468f
zeeshanmcp12 <zeeshanmcp12@gmail.com>
2022-01-28 20:04:03 +0500

    basic example added for slices

---
+++ 09slices/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Learning about Slices in golang!")
+
+	var sliceList = []string{}
+	fmt.Printf("Type of sliceList var is %T", sliceList)
+
+}
