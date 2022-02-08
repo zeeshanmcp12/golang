101ccdd65971a97da84abaec212ae91731c312a1
zeeshanmcp12 <zeeshanmcp12@gmail.com>
2022-02-09 02:20:15 +0500

    making learning fun

--- funExamples/main.go
+++ funExamples/main.go
@@ -9,22 +9,30 @@ import (
 )
 
 func main() {
-	fmt.Println("This is about fun excersices!")
-	var funnyItems = []string{"Looks good!", "You're cool", "no way", "Awesome", "Beautiful"}
-	fmt.Println(funnyItems)
-	// fmt.Println(len(funnyItems))
+	fmt.Println("This is about fun exercises!")
+	var funnyItems = []string{"Looks good!", "That's really cool", "No way", "Awesome", "Beautiful"}
+	// fmt.Println(funnyItems)
+	fmt.Println("------------------------------")
 
 	reader := bufio.NewReader(os.Stdin)
-	fmt.Printf("Enter number from 1 to 4 to see something cool: ")
+	fmt.Printf("Enter a number from 0 to 4 to see something cool: ")
 	input, _ := reader.ReadString('\n')
-	fmt.Printf(input)
+	fmt.Println("Thank you for entering:", input)
+	fmt.Println("------------------------------")
+
 	convertToInt, err := strconv.Atoi(strings.TrimSpace(input))
 	if err != nil {
 		fmt.Println(err)
 	} else {
 		var index = convertToInt
 		fmt.Println(funnyItems[index])
+		fmt.Println("------------------------------")
+
 		funnyItems = append(funnyItems[:index], funnyItems[index+1:]...)
 		fmt.Println("Remaining items are: ", funnyItems)
+		fmt.Println()
 	}
+	fmt.Printf("Press enter key to continue...")
+	pressAnyKey, _ := reader.ReadString('\n')
+	fmt.Println(pressAnyKey)
 }
