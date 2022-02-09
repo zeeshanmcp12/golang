537d45fe18f1f4985d181212711689af53c499eb
zeeshanmcp12 <zeeshanmcp12@gmail.com>
2022-02-09 19:38:08 +0500

    add basic example for structs

--- 11structs/main.go
+++ 11structs/main.go
@@ -4,4 +4,15 @@ import "fmt"
 
 func main() {
 	fmt.Println("Learn & Practice Structs in golang!")
+	user := User{"Zeeshan", "acloudtechie@outlook.com", "DevOPS Engineer", 32}
+	fmt.Printf("User details are: %v\n", user)
+	fmt.Printf("User details are: %+v\n", user)
+	fmt.Printf("Username: %v", user.Name)
+}
+
+type User struct {
+	Name       string
+	Email      string
+	Profession string
+	Age        int
 }
