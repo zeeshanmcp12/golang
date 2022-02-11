4697fd220ad7325776387b2b66064ed54c818ddf
zeeshanmcp12 <zeeshanmcp12@gmail.com>
2022-02-11 23:54:36 +0500

    code refactoring

--- funExamples/login.go
+++ funExamples/login.go
@@ -16,30 +16,18 @@ func main() {
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Printf("Enter your Name: ")
 	username, _ := reader.ReadString('\n')
-	fmt.Printf("Enter Email Address: ")
-	emailAddress, _ := reader.ReadString('\n')
-	// fmt.Printf("Enter your Age: ")
-	// age, _ := reader.ReadString('\n')
-
-	// Convert input to string/int
-	// Age converted to Int because it was string before
-	// convertedAge, _ := strconv.Atoi(strings.TrimSpace(age))
 
 	// Trim space from string because of ReadString function above
 	convertedUsername := strings.TrimSpace(username)
-	convertedEmail := strings.TrimSpace(emailAddress)
 
 	// Utilize elements from structure
-	userProfile := User{convertedUsername, convertedEmail}
+	userProfile := User{convertedUsername}
 
 	if userProfile.Name == userDb[0] {
 		fmt.Printf("Thank you for login, %v", userProfile.Name)
 		fmt.Println()
-	} else if userProfile.Name != userDb[0] {
-		fmt.Printf("Your provided %v value does not exist in our system.\n", userProfile.Name)
-
 	} else {
-		fmt.Println("Enter correct details!")
+		fmt.Println("Enter correct details")
 
 	}
 	/*
@@ -55,6 +43,5 @@ func main() {
 
 // Define Structure
 type User struct {
-	Name  string
-	Email string
+	Name string
 }
