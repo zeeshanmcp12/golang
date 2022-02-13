9657e2ab9f10665a04fa2f6621809db541e8644c
zeeshanmcp12 <zeeshanmcp12@gmail.com>
2022-02-14 00:35:46 +0500

    if else example added in 00testing

--- 00testing/main.go
+++ 00testing/main.go
@@ -4,20 +4,41 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
 func main() {
 	fmt.Println("New file for practice!")
+	/*	reader := bufio.NewReader(os.Stdin)
+		fmt.Printf("Enter your name: ")
+		input, _ := reader.ReadString('\n')
+
+		c_userName := strings.TrimSpace(input)
+		fmt.Println(c_userName)
+
+		userDb := []string{}
+		userDb = append(userDb, c_userName)
+		fmt.Printf("Value in userDb is %v", userDb)*/
+	/*if num := 10; num < 10 {
+		fmt.Println("Number is less than 10")
+	} else if num > 10 {
+		fmt.Println("Number is NOT less than 10")
+	} else {
+		fmt.Println("Number is equal")
+	}*/
+
 	reader := bufio.NewReader(os.Stdin)
-	fmt.Printf("Enter your name: ")
+	fmt.Printf("Enter any number to compare: ")
 	input, _ := reader.ReadString('\n')
-
-	c_userName := strings.TrimSpace(input)
-	fmt.Println(c_userName)
-
-	userDb := []string{}
-	userDb = append(userDb, c_userName)
-	fmt.Printf("Value in userDb is %v", userDb)
-
+	c_number, _ := strconv.Atoi(strings.TrimSpace(input))
+	fmt.Printf("Thank you for entering: %v", c_number)
+	fmt.Println()
+	if num := 5; c_number < num {
+		fmt.Printf("Your number %v is less than %v defined in if/else", c_number, num)
+	} else if c_number > num {
+		fmt.Printf("Your number %v is NOT than %v defined in if/else", c_number, num)
+	} else {
+		fmt.Printf("Both %v and %v Number is equal", c_number, num)
+	}
 }
