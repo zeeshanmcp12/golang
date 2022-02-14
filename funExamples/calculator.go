07241fb7601b56523cd95fe51d818a81f477f78a
zeeshanmcp12 <zeeshanmcp12@gmail.com>
2022-02-14 20:49:17 +0500

    write simple calculator logic

---
+++ funExamples/calculator.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	fmt.Println("----------Basic Caluclator----------")
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Printf("Enter first number: ")
+	firstNum, _ := reader.ReadString('\n')
+	fmt.Printf("Enter second number: ")
+	secondNum, _ := reader.ReadString('\n')
+	fmt.Printf("Enter any of these operator, sum, sub, mul, div: ")
+	calculate, _ := reader.ReadString('\n')
+
+	numOne, err := strconv.Atoi(strings.TrimSpace(firstNum))
+	numTwo, err := strconv.Atoi(strings.TrimSpace(secondNum))
+
+	var operator string = strings.TrimSpace(calculate)
+
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Printf("Thank you for entering first number %v ", numOne)
+		fmt.Println()
+		fmt.Printf("Thank you for entering second number %v ", numTwo)
+		fmt.Println()
+
+		switch operator {
+		case "sum":
+			fmt.Println("Sum of two number: ", numOne+numTwo)
+		case "sub":
+			fmt.Println("Subtraction of two number: ", numOne-numTwo)
+		case "mul":
+			fmt.Println("Product of two number: ", numOne*numTwo)
+		case "div":
+			fmt.Println("Division of two number: ", numOne/numTwo)
+		default:
+			fmt.Println("Default statement executed!")
+
+		}
+	}
+
+}
