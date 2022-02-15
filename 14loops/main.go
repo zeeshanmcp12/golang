5338dcfd411ec9e2d0b01121782b3d5e8447be47
zeeshanmcp12 <zeeshanmcp12@gmail.com>
2022-02-15 18:16:47 +0500

    working with loops

---
+++ 14loops/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("----------Loops in Golang----------\n")
+
+	days := []string{"Saturday", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"}
+	fmt.Println(days)
+}
