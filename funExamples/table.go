5b449f1790df86a75d43580744d628524661b243
zeeshanmcp12 <zeeshanmcp12@gmail.com>
2022-02-18 23:20:29 +0500

    loops in golang, print table

--- funExamples/table.go
+++ funExamples/table.go
@@ -3,10 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Loops in golang")
-	// table := 2
+	fmt.Println("Print Table")
 	for d := 1; d < 10+1; d++ {
 		fmt.Printf("2 x %v = %v\n", d, d*2)
-
 	}
 }
