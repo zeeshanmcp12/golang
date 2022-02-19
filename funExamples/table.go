1ed6ec3dfb087b4686ab4a55809a52de7a8f0099
zeeshanmcp12 <zeeshanmcp12@gmail.com>
2022-02-19 18:20:23 +0500

    table logic for my son

--- funExamples/table.go
+++ funExamples/table.go
@@ -1,10 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
 func main() {
 	fmt.Println("Print Table")
-	for d := 1; d < 10+1; d++ {
-		fmt.Printf("2 x %v = %v\n", d, d*2)
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Printf("Enter any number to print table: ")
+	input, _ := reader.ReadString('\n')
+	c_table_num, err := strconv.Atoi(strings.TrimSpace(input))
+
+	// table_num := c_table_num * 2
+
+	if err != nil {
+		fmt.Println(err)
+	} else {
+
+		for d := 1; d < 10+1; d++ {
+			fmt.Printf("%v x %v = %v\n", c_table_num, d, c_table_num*d)
+		}
 	}
+
+	fmt.Printf("Press enter to continue....")
+	pressAnyKey, _ := reader.ReadString('\n')
+	fmt.Println(pressAnyKey)
 }
