9a8ec641dd2a1fdf4e81e49fa9f01c3a56d8e884
zeeshanmcp12 <zeeshanmcp12@gmail.com>
2022-02-20 01:23:03 +0500

    different types of loop

--- 14loops/main.go
+++ 14loops/main.go
@@ -25,9 +25,24 @@ func main() {
 	*/
 	// for loop as while loop
 	// loop will keep run until the condition is true.
-	number := 0
+	/*number := 0
 	for number < 5 {
 		fmt.Println(number)
 		number++
+	}*/
+
+	days := []string{"Saturday", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Firday"}
+	// for d := 0; d < len(days); d++ {
+	// 	fmt.Println(days[d])
+	// }
+
+	// If we want to loop through the entire range then the prefered method is to create for loop with range keyword.
+	for i := range days {
+		fmt.Println(days[i])
+	}
+
+	// If we want index and it's value together then we can use below format.
+	for index, day := range days {
+		fmt.Printf("Index is %v and Value is %v\n", index+1, day)
 	}
 }
