2372ac039f7f753df3262972a9ee3aedb4c6b265
zeeshanmcp12 <zeeshanmcp12@gmail.com>
2022-02-23 01:55:11 +0500

    todo app progress

--- funExamples/todo.go
+++ funExamples/todo.go
@@ -11,12 +11,33 @@ func main() {
 	fmt.Println("Todo app")
 	todos := []string{}
 
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Printf("Add todo: ")
-	input, _ := reader.ReadString('\n')
-	fmt.Println(input)
-	trimSpace := strings.TrimSpace(input)
-	todos = append(todos, trimSpace)
-	fmt.Println(todos)
+	for i := 0; true; i++ {
+
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Printf("Add todo: ")
+		input, _ := reader.ReadString('\n')
+		// fmt.Println(input)
+		f_text := strings.TrimSpace(input)
+		todos = append(todos, f_text)
+		fmt.Println(f_text)
+
+		if f_text != "Done" {
+			fmt.Printf("Enter another task or type 'Done' when you're done: ")
+			moreTodos, _ := reader.ReadString('\n')
+			mt_f_text := strings.TrimSpace(moreTodos)
+			todos = append(todos, mt_f_text)
+			fmt.Printf("Current tasks %v\n", todos)
+			continue
+
+		} else if f_text == "Done" {
+			fmt.Printf("Your Todos %v\n", todos)
+			break
+		} else {
+			fmt.Println("Invalid input!")
+		}
+
+	}
+
+	// fmt.Println(todos)
 
 }
