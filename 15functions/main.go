9ce4268120f18815f5964709adec8b32c1b4fb3e
zeeshanmcp12 <zeeshanmcp12@gmail.com>
2022-02-24 00:10:04 +0500

    function example

--- 15functions/main.go
+++ 15functions/main.go
@@ -2,9 +2,15 @@ package main
 
 import "fmt"
 
+func add(numOne, numTwo int) {
+	fmt.Println(numOne + numTwo)
+
+}
+
 func main() {
 	fmt.Println("functions in golang")
 	greetings()
+	add(5, 5)
 }
 
 func greetings() {
