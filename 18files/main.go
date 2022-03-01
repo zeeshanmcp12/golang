039bde552f84bbf1e9b75bc01e2241205bb5c7d4
zeeshanmcp12 <zeeshanmcp12@gmail.com>
2022-03-02 01:09:35 +0500

    add comments for others to understand

--- 18files/main.go
+++ 18files/main.go
@@ -11,19 +11,27 @@ import (
 
 func main() {
 	fmt.Println("Working with files in golang!")
+
+	// Taking user input by calling userInput() funcion
 	content := userInput()
 
+	// Creating file
 	file, err := os.Create("./userData.txt")
 
+	// Writing content to file
 	io.WriteString(file, content)
 
 	CheckNilErr(err)
+
+	// Reading data from the file created above
 	readFile("./userData.txt")
 
+	// Closing file at the end of execution
 	defer file.Close()
 
 }
 
+// function to take input from user
 func userInput() string {
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Print("Enter any text to add in file: ")
@@ -33,12 +41,14 @@ func userInput() string {
 
 }
 
+// Check if err has something in it
 func CheckNilErr(err error) {
 	if err != nil {
 		panic(err)
 	}
 }
 
+// ReadFile function
 func readFile(fileData string) {
 	data, _ := ioutil.ReadFile(fileData)
 	fmt.Print("File to be written with data\n", string(data))
