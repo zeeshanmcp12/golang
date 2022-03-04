4c9e86b09002b74ac6ee65495044dc5f2736bedc
zeeshanmcp12 <zeeshanmcp12@gmail.com>
2022-03-05 02:18:22 +0500

    some practice on exercism

---
+++ exercism-excercies/1_lasagna_min.go
@@ -0,0 +1,28 @@
+package lasagna
+
+import "fmt"
+
+// TODO: define the 'OvenTime' constant
+const OvenTime = 40
+
+func main() {
+	fmt.Println("Practice from exercism!")
+	RemainingOvenTime()
+}
+
+// RemainingOvenTime returns the remaining minutes based on the `actual` minutes already in the oven.
+func RemainingOvenTime(actualMinutesInOven int) int {
+	panic("RemainingOvenTime not implemented")
+	result := actualMinutesInOven - OvenTime
+	return result
+}
+
+// PreparationTime calculates the time needed to prepare the lasagna based on the amount of layers.
+func PreparationTime(numberOfLayers int) int {
+	panic("PreparationTime not implemented")
+}
+
+// ElapsedTime calculates the total time needed to create and bake a lasagna.
+func ElapsedTime(numberOfLayers, actualMinutesInOven int) int {
+	panic("ElapsedTime not implemented")
+}
---
+++ exercism-excercies/go.mod
@@ -0,0 +1,3 @@
+module excersim
+
+go 1.17
