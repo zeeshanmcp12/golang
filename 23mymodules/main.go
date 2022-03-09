3c7cce5e28ffc69659cad4fb0f24248e7dd463f5
zeeshanmcp12 <zeeshanmcp12@gmail.com>
2022-03-10 01:08:58 +0500

    receive json data and print on html page

--- 23mymodules/main.go
+++ 23mymodules/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -11,12 +12,28 @@ import (
 func main() {
 	fmt.Println("Learning Go Modules!")
 	r := mux.NewRouter()
-	r.HandleFunc("/", serveHome).Methods("GET")
+	r.HandleFunc("/", serveHome).Methods("GET") //here we are using reference of function serveHome and not the actual function.
 	fmt.Println("Listening on http://localhost:8000")
 	log.Fatal(http.ListenAndServe(":8000", r))
+
 }
 
 func serveHome(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("<h1>Learning Modules in golang</h1>"))
+	jsonData := []Profile{
+		{"Zeeshan", 32, "acloudtechie.com"},
+		{"Abdullah", 30, "abc.com"},
+	}
 
+	finalJSON, _ := json.MarshalIndent(jsonData, "", "\t")
+
+	fmt.Println(string(finalJSON))
+
+	w.Write([]byte(finalJSON))
+
+}
+
+type Profile struct {
+	Name    string
+	Age     int
+	Website string
 }
