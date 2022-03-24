2249c42e2e70a0d71ee968cf95f0b24e69ef67f8
zeeshanmcp12 <zeeshanmcp12@gmail.com>
2022-03-25 01:14:21 +0500

    todo app logic

--- 00practice/main.go
+++ 00practice/main.go
@@ -16,26 +16,55 @@ func main() {
 		for i := 1; i < 10+1; i++ {
 			fmt.Printf("%v x %v = %v\n", tableNum, i, tableNum*i)
 		}*/
+	/*
+		fmt.Printf("Enter any number to make table: ")
+		tableNum := userInput()
 
-	fmt.Printf("Enter any number to make table: ")
-	tableNum := userInput()
+		for i := 1; i < 10+1; i++ {
 
-	for i := 1; i < 10+1; i++ {
+			// anotherInt := i * 2
+			fmt.Printf("%v x %v = ", tableNum, i)
+			result := userInput()
 
-		// anotherInt := i * 2
-		fmt.Printf("%v x %v = ", tableNum, i)
-		result := userInput()
+			// Working logic
+			if result == tableNum*i {
+				fmt.Println("result executed")
 
-		// Working logic
-		if result == tableNum*i {
-			fmt.Println("result executed")
+			} else {
+				fmt.Printf("Wrong Answer!\n%v x %v = %v\n", tableNum, i, userInput())
+			}
 
-		} else {
-			fmt.Printf("Wrong Answer!\n%v x %v = %v\n", tableNum, i, userInput())
+		}*/
+
+	todos := []string{}
+	fmt.Printf("Add task: ")
+
+	for i := 0; true; i++ {
+
+		f_text := strings.TrimSpace(stringInput())
+
+		// fmt.Printf("Add task or Enter done: ")
+		if f_text != "done" {
+			if f_text != "" {
+
+				fmt.Printf("Add task or enter done: ")
+				todos = append(todos, f_text)
+				continue
+
+			} else {
+				fmt.Printf("Invalid input. Add task or enter done: ")
+
+			}
+
+		} else if f_text == "done" {
+			fmt.Println()
+			fmt.Println("Here is your tasks:")
+			for j, val := range todos {
+				fmt.Printf("%v %v\n", j+1, val)
+			}
+			break
 		}
-
 	}
-
 }
 
 func userInput() int {
