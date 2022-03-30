e9794089a13c8cfd8d3489824fbb30799052e09e
zeeshanmcp12 <zeeshanmcp12@gmail.com>
2022-03-30 19:56:16 +0500

    gorilla mux router

--- testing/main.go
+++ testing/main.go
@@ -1,12 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 func main() {
 	fmt.Println("Testing file")
+
+	greeter()
+	r := mux.NewRouter()
+	r.HandleFunc("/", serveHome).Methods("GET")
+	fmt.Println("Listening on port 4000")
+	log.Fatal(http.ListenAndServe(":4000", r))
 
 }
 
 func greeter() {
 	fmt.Println("Hello from golang!")
+}
+
+func serveHome(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("<h1>Welcome to A Cloud Techie!</h1>"))
+
 }
