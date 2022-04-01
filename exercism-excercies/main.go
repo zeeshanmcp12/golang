108f8e60d8c13a27d2c749f3565630efa7e2b3df
zeeshanmcp12 <zeeshanmcp12@gmail.com>
2022-04-01 18:47:26 +0500

    exercism challenge

--- exercism-excercies/main.go
+++ exercism-excercies/main.go
@@ -7,6 +7,7 @@ import "fmt"
 	fmt.Printf("the type of intelligenceQuotient is %T\n", intelligenceQuotient)
 }*/
 
+/*
 func main() {
 	const secondsInMinute = 60
 	const minutesInHour = 60
@@ -14,4 +15,39 @@ func main() {
 
 	// don't edit below this line
 	fmt.Println("number of seconds in an hour:", secondsInHour)
+}*/
+
+const OvenTime = 40
+
+func main() {
+	fmt.Printf("Lasagna preparation time is %v mins\n", OvenTime)
+	fmt.Printf("Remaining minutes in oven: %v \n", RemainingOvenTime(30))
+	fmt.Printf("Time spent on preparation: %v mins\n", PreparationTime(1))
+	fmt.Println("Calling from main function:", ElapsedTime(1, 30))
+
+	// fmt.Printf("Total Elapsed time in %v mins\n", ElapsedTime(3, 15))
+
+}
+
+func RemainingOvenTime(actualMinutesInOven int) int {
+	return OvenTime - actualMinutesInOven
+
+}
+
+func PreparationTime(numberOfLayers int) int {
+	return numberOfLayers * 2
+}
+
+func ElapsedTime(numberOfLayers, actualMinutesInOven int) int {
+	// numoflayer := PreparationTime(numOfLayers)
+	// timeSpent := RemainingOvenTime(actualMinsInOven)
+
+	// return numoflayer + timeSpent
+	// fmt.Printf("Total elapsed time %v\n", PreparationTime(numberOfLayers)+RemainingOvenTime(actualMinutesInOven))
+	// result := PreparationTime(numberOfLayers) + RemainingOvenTime(actualMinutesInOven)
+	// fmt.Printf("Total elapsed time %v\n", result)
+
+	return PreparationTime(numberOfLayers) + RemainingOvenTime(actualMinutesInOven)
+	// return RemainingOvenTime(actualMinutesInOven) + PreparationTime(numberOfLayers)
+
 }
