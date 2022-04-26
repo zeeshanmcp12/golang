07bc3cb584d696720ef3eed5fd80279257ff6e28
zeeshanmcp12 <zeeshanmcp12@gmail.com>
2022-04-26 15:53:48 +0500

    some notes for table logic

--- 00notesexample/return-types.go
+++ 00notesexample/return-types.go
@@ -7,7 +7,8 @@ func main() {
 
 	fmt.Println("Sum of 10 and 20 is", sumNumbers(10, 20))
 	table(0, 1, 2, 3, 4, 5, 6) // It works without any "index out of range" error
-	// table(1, 2, 3, 4, 5, 6) // It thwos "index out of range" error
+	// table(1, 2, 3, 4, 5, 6) // It throws "index out of range" error...I couldn't understand why, this is because of slice and array indexing
+	// useful maybe https://stackoverflow.com/questions/39118941/go-panic-runtime-error-index-out-of-range-when-the-length-of-array-is-not-nu
 
 }
 
@@ -15,7 +16,10 @@ func table(numbers ...int) {
 
 	for _, val := range numbers {
 		for i := 1; i < 10+1; i++ {
-			fmt.Printf("%d x %d = %d\t", numbers[val], i, numbers[val]*i)
+			// fmt.Println("Capacity of numbers", cap(numbers))
+			fmt.Printf("%d x %d = %d\t ", numbers[val], i, numbers[val]*i)
+			// fmt.Println("Index =>", index, "Value =>", val, "Length =>", len(numbers), "Capacity => ", cap(numbers))
+
 		}
 	}
 }
