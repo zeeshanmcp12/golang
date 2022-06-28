28c32500dad9e94628d3e3efa677b5a0c0b54ad8
zeeshanmcp12 <zeeshanmcp12@gmail.com>
2022-06-28 11:36:44 +0500

    main-prac with for loop

--- 14loops/main-prac.go
+++ 14loops/main-prac.go
@@ -5,9 +5,13 @@ import "fmt"
 func main() {
 
 	fmt.Println("Loops in golang!")
+	var tableNum int
+
+	fmt.Print("Enter any number: ")
+	fmt.Scanf("%v\n", &tableNum)
 
 	for i := 1; i < 10+1; i++ {
-		fmt.Printf("2 x %v = %v\n", i, i*2)
+		fmt.Printf("%v x %v = %v\n", tableNum, i, i*tableNum)
 
 	}
 }
