980bac5b0af2ae6d1c94a5a3c0ae9ddde7bcabdd
zeeshanmcp12 <zeeshanmcp12@gmail.com>
2022-06-28 12:41:21 +0500

    for loop in a style of while loop

--- 14loops/main-prac.go
+++ 14loops/main-prac.go
@@ -1,17 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 
 	fmt.Println("Loops in golang!")
-	var tableNum int
+	// var tableNum int
 
-	fmt.Print("Enter any number: ")
-	fmt.Scanf("%v\n", &tableNum)
+	// fmt.Print("Enter any number: ")
+	// fmt.Scanf("%v\n", &tableNum)
 
-	for i := 1; i < 10+1; i++ {
-		fmt.Printf("%v x %v = %v\n", tableNum, i, i*tableNum)
+	// for i := 1; i < 10+1; i++ {
+	// 	fmt.Printf("%v x %v = %v\n", tableNum, i, i*tableNum)
+
+	// }
+
+	var number int = 20
+
+	// time.Now().UnixNano()
+
+	for {
+		if number == 20 {
+			fmt.Println(number)
+		}
+		number++
+		fmt.Println(number)
+		if number >= 100 {
+			fmt.Println(number)
+			break
+		}
 
 	}
+
 }
