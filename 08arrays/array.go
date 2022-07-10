53b8f4bf7d310a88496d4bb4069bc009e99c09b3
zeeshanmcp12 <zeeshanmcp12@gmail.com>
2022-07-10 11:13:44 +0500

    array with loops

--- 08arrays/array.go
+++ 08arrays/array.go
@@ -6,14 +6,20 @@ func main() {
 	fmt.Println("Array in golang!")
 
 	var list [4]string
-	list[0] = "item1"
-	list[1] = "item2"
-	list[2] = "item3"
+	list[0] = "Apple"
+	list[1] = "Potatoe"
+	list[2] = "Orange"
+	list[3] = "Banana"
 
-	fmt.Println("Elements in an array: ", len(list))
-	fmt.Println("Total items in the list: ", list)
+	// fmt.Println("Elements in an array: ", len(list))
+	// fmt.Println("Total items in the list: ", list)
 
-	var anotherList = [4]string{"item4", "item5", "item6", "item7"}
-	fmt.Println(anotherList)
+	// var anotherList = [4]string{"item4", "item5", "item6", "item7"}
+	// fmt.Println(anotherList)
+
+	for i := 0; i < len(list); i++ {
+		fmt.Printf("Item # %v is %v\n", i, list[i])
+
+	}
 
 }
