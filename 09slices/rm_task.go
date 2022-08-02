00d115dcd701a8984037d093c7aa003cbe7e854b
zeeshanmcp12 <zeeshanmcp12@gmail.com>
2022-08-02 11:44:56 +0500

    rm_task updated

--- 09slices/rm_task.go
+++ 09slices/rm_task.go
@@ -3,16 +3,28 @@ package main
 import "fmt"
 
 func main() {
-	tasks := []string{"1: Task1", "2: Task2", "3: Task3", "4: Task4"}
-	fmt.Println(tasks)
+	tasks := []string{"Morning Walk", "Workout", "Learn to code", "Write some lines"}
+	// fmt.Println(tasks)
+	for j, todo := range tasks {
+		fmt.Printf("%v task: %v\n", j, todo)
+	}
 
 	var index int
 
 	fmt.Print("Select any number to delete task: ")
 	fmt.Scanf("%v", &index)
 
+	// index += 0
+
+	// fmt.Println("Value in index: ", index)
+
 	tasks = append(tasks[:index], tasks[index+1:]...)
-	for j, todo := range tasks {
+
+	// still working on it
+	var updated_task []string = tasks[index:]
+	// fmt.Println("Updated tasks: ", updated_task)
+
+	for j, todo := range updated_task {
 		fmt.Printf("%v task: %v\n", j, todo)
 	}
 
