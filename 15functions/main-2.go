a0b99a61f96c5ac33fe207f609d5c2466df06b6d
zeeshanmcp12 <zeeshanmcp12@gmail.com>
2022-08-19 17:00:54 +0500

    strToInt func refactored

--- 15functions/main-2.go
+++ 15functions/main-2.go
@@ -11,28 +11,40 @@ import (
 func main() {
 	fmt.Println("Handling user input using function")
 
+	fmt.Printf("Enter any text: ")
+
 	result := strings.TrimSpace(userInput())
-	fmt.Println("Your input:", result)
+	fmt.Printf("Input: %v, Type: %T", result, result)
 
-	fmt.Printf("Type of input: %T", result)
+	fmt.Println()
 
-	// fmt.Println()
-	// fmt.Printf("Tsype of input %T", userInput())
+	fmt.Print("Enter any number: ")
+	text := userInput()
+
+	fmt.Printf("Type of text is %T\n", text)
+
+	fmt.Printf("Input: %v, Type: %T", strToInt(text), strToInt(text))
 
 }
 
 func userInput() string {
 	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("Enter any text: ")
 	input, _ := reader.ReadString('\n')
 	return input
 }
 
 func strToInt(strText string) int {
-	input, err := strconv.Atoi(strText)
+	input, err := strconv.Atoi(strings.TrimSpace(strText))
+	// if err != nil {
+	// 	panic(err)
+	// }
+	checkErr(err)
+	return input
+
+}
+
+func checkErr(err error) {
 	if err != nil {
 		panic(err)
 	}
-	return input
-
 }
