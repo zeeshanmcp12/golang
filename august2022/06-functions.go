3da89bff92bfc5aa25c9349f20f1f42a76f54516
zeeshanmcp12 <zeeshanmcp12@gmail.com>
2022-08-26 13:09:05 +0500

    working on struct

--- august2022/06-functions.go
+++ august2022/06-functions.go
@@ -1,21 +1,24 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
-	"strings"
 )
 
 func main() {
 	fmt.Println("Using function in golang!")
 
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("Enter your name: ")
-	input, _ := reader.ReadString('\n')
-	f_text := strings.TrimSpace(input)
+	// reader := bufio.NewReader(os.Stdin)
+	// fmt.Print("Enter your name: ")
+	// input, _ := reader.ReadString('\n')
+	// f_text := strings.TrimSpace(input)
 
-	greeter(f_text)
+	// greeter(f_text)
+
+	platformUser := StringData{}
+
+	// platformUser.GetDataFromStruct(GetInput("Zeeshan"))
+	platformUser.GetDataFromStruct("Zeeshan")
+
 }
 
 func greeter(input string) int {
@@ -25,5 +28,24 @@ func greeter(input string) int {
 	fmt.Printf("Characters in your name %v", charInName)
 	return charInName
 	// return charInName
+
+}
+
+type StringData struct {
+	Name string
+	// Email string
+	// Age   int
+}
+
+func (s StringData) GetDataFromStruct(input string) string {
+	fmt.Print("Enter anything: ", GetInput(input))
+	return s.Name
+
+}
+
+func GetInput(input string) string {
+	// var text string
+	fmt.Scanf("%v", &input)
+	return input
 
 }
