11fe4967907ce33c97fee8a06d578e6aaceee83c
zeeshanmcp12 <zeeshanmcp12@gmail.com>
2022-09-02 16:35:24 +0500

    function revisit

---
+++ september2022/06-03-functions.go
@@ -0,0 +1,46 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("using functions in golang!")
+
+	fmt.Println("Sum is: ", adder(10, 10))
+
+	fmt.Println()
+
+	// Calling Variadic function
+	fmt.Println("Executing variadic function!")
+	fmt.Println("Adding no num: ", variadicFun(0))
+	fmt.Println("Adding 2 numbers: ", variadicFun(2, 4))
+	fmt.Println("Adding 3 numbers: ", variadicFun(2, 3, 4))
+
+	fmt.Println()
+
+	// Anonymous function
+	fmt.Println("Executing anonymous function!")
+	result := func(num1, num2 int) int {
+		sum := num1 + num2
+		return sum
+	}(10, 20)
+
+	fmt.Printf("Type of result: %T\n", result)
+	fmt.Println("Sum is", result)
+
+	fmt.Println()
+
+}
+
+func adder(num1, num2 int) int {
+	sum := num1 + num2
+	return sum
+}
+
+func variadicFun(number ...int) int {
+	sum := 0
+
+	for _, value := range number {
+		sum += value
+	}
+	return sum
+}
