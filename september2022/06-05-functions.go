83b107bed29e12cbcd4e570a4bf1065b20d291d8
zeeshanmcp12 <zeeshanmcp12@gmail.com>
2022-09-06 11:58:40 +0500

    complete high order function

--- september2022/06-05-functions.go
+++ september2022/06-05-functions.go
@@ -14,10 +14,45 @@ import "fmt"
 
 func main() {
 	fmt.Println("Using High order function in golang!")
-	fmt.Println("Area of the circle: ", calculateArea(9.1))
+
+	var (
+		radius float64
+		query  int
+	)
+
+	fmt.Printf("Enter radius of a circle: ")
+	fmt.Scanf("%f\n", &radius)
+	fmt.Println()
+	fmt.Print("Select any number to calculate:\n1: Area\n2: Perimeter\n3: Diameter: ")
+	fmt.Scanf("%v\n", &query)
+
+	printResult(radius, getFunction(query))
 }
 
-func calculateArea(radius float64) float64 {
-	result := 3.14 * radius * radius
-	return result
+func printResult(radius float64, calcFunction func(radius float64) float64) {
+	result := calcFunction(radius)
+	fmt.Println("Result: ", result)
+	fmt.Println("Thank you!")
+
+}
+
+func getFunction(selection int) func(radius float64) float64 {
+	selection_to_func := map[int]func(radius float64) float64{
+		1: calcArea,
+		2: calcPerimeter,
+		3: calcDiameter,
+	}
+	return selection_to_func[selection]
+}
+
+func calcArea(radius float64) float64 {
+	return 3.14 * radius * radius
+}
+
+func calcDiameter(radius float64) float64 {
+	return 2 * 3.14 * radius
+}
+
+func calcPerimeter(radius float64) float64 {
+	return 2 * radius
 }
