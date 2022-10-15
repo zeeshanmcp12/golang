9aec053c0ffb11bd90bd32a42a19cb7c65d262d5
zeeshanmcp12 <zeeshanmcp12@gmail.com>
2022-10-15 14:31:05 +0500

    take inpurt from user and store in struct

--- october2022/2-struct.go
+++ october2022/2-struct.go
@@ -4,4 +4,32 @@ import "fmt"
 
 func main() {
 	fmt.Println("Revisiting structs in golang!")
+
+	var (
+		name        string
+		age         int
+		isConfirmed bool
+	)
+
+	fmt.Print("Enter your name: ")
+	fmt.Scanf("%v\n", &name)
+
+	fmt.Print("Enter your age: ")
+	fmt.Scanf("%v\n", &age)
+
+	fmt.Print("Do you agree? (true/false) ")
+	fmt.Scanf("%t\n", &isConfirmed)
+
+	userData := User{name, age, isConfirmed}
+
+	// fmt.Println("Hi", userData.Name, "glad to know you are", userData.Age, "years old.")
+	fmt.Printf("Hi %q, glad to know you are %v years old.", userData.Name, userData.Age)
+
+	// fmt.Printf("Type of struct is %T", userData)
+}
+
+type User struct {
+	Name       string
+	Age        int
+	isLoggedIn bool
 }
