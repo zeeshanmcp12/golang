4cfb4a0808febab52287fe1c28000405d59ba0aa
zeeshanmcp12 <zeeshanmcp12@gmail.com>
2022-10-20 12:01:17 +0500

    create data structure for user data

---
+++ october2022/4-struct.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Data structure in golang using struct!")
+
+	var (
+		name, email string
+		age         int
+	)
+
+	fmt.Print("Enter your name: ")
+	fmt.Scanf("%v\n", &name)
+
+	fmt.Print("Enter your email: ")
+	fmt.Scanf("%v\n", &email)
+
+	fmt.Print("Enter your age: ")
+	fmt.Scanf("%v\n", &age)
+
+	// userData := User{"Zeeshan", "abc@go.dev", 30}
+
+	userData := User{name, email, age}
+
+	fmt.Printf("Hey %v, your email address is: %v\n", userData.Name, userData.Email)
+	fmt.Printf("Glad to know that you are %v years old.", userData.Age)
+
+	// fmt.Println(userData.Name)
+
+}
+
+type User struct {
+	Name  string
+	Email string
+	Age   int
+}
