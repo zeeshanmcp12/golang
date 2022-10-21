2ca5b839f47af14cfe77a330d3904ac13f965694
zeeshanmcp12 <zeeshanmcp12@gmail.com>
2022-10-21 11:06:42 +0500

    match your number with random number

--- october2022/5-switch-case.go
+++ october2022/5-switch-case.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
 func main() {
 	fmt.Println("Switch case in golang!")
@@ -9,13 +13,16 @@ func main() {
 
 	fmt.Print("Enter any number: ")
 	fmt.Scanf("%v", &userInput)
-	fmt.Printf("Thank you for input: %v\n", userInput)
+
+	rand.Seed(time.Now().UnixNano())
+	randNum := rand.Intn(10)
+
+	fmt.Printf("Your number: %v\n", userInput)
+	fmt.Printf("Random number: %v\n", randNum)
 
 	switch userInput {
-	case 5:
-		fmt.Printf("Case 5 matched with your number %v", userInput)
-	case 3:
-		fmt.Printf("Case 3 matched with your number %v", userInput)
+	case randNum:
+		fmt.Printf("Congratulations! Random no: %v matched with your number %v", randNum, userInput)
 	default:
 		fmt.Println("Number doesn't match so default statement printed!")
 	}
