f2b80bb768f369ec985448e06a7cb7cfa4879261
zeeshanmcp12 <zeeshanmcp12@gmail.com>
2022-10-22 13:50:51 +0500

    write and call anonymous function

--- october2022/7-function.go
+++ october2022/7-function.go
@@ -5,13 +5,31 @@ import "fmt"
 func main() {
 	fmt.Println("functions in golang!")
 
-	// fmt.Println()
-
 	var numOne, numTwo int
 	fmt.Printf("Enter two numbers: ")
 	fmt.Scanf("%v,%v", &numOne, &numTwo)
-	fmt.Printf("Sum of %v and %v is %v", numOne, numTwo, proAdder(numOne, numTwo))
-	fmt.Println()
+
+	// Calling proAdder function here
+	fmt.Printf("Sum of %v and %v is %v\n", numOne, numTwo, proAdder(numOne, numTwo))
+	fmt.Println("----------------------")
+
+	// Writing and calling anonymous function
+	anonymousfuncv1 := func(val1, val2 int) int {
+		return val1 + val2
+	}(3, 4)
+
+	fmt.Printf("Type of anonymousfuncv1 func %T\n", anonymousfuncv1)
+	fmt.Println("Result of anonymousfuncv1: ", anonymousfuncv1)
+	fmt.Println("----------------------")
+
+	// Writing and calling anonymous function v2
+	anonymousfuncv2 := func(num1, num2 int) int {
+		return num1 + num2
+	}
+
+	fmt.Printf("Type of anonymousfuncv2 is %T\n", anonymousfuncv2)
+	fmt.Printf("Result of aanonymousfuncv2 is: %v\n", anonymousfuncv2(2, 3))
+	fmt.Println("----------------------")
 
 	// add()
 
