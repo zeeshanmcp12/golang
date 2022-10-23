ab097b69b5aa9a6532f856de82229456dbe160a8
zeeshanmcp12 <zeeshanmcp12@gmail.com>
2022-10-23 15:33:15 +0500

    update email address

--- october2022/8-method.go
+++ october2022/8-method.go
@@ -6,18 +6,26 @@ func main() {
 	fmt.Println("Method in golang!")
 	fmt.Println("When we use 'Struct' in function, it is called method.")
 
-	userData := User{"Zeeshan", 30, true}
+	userData := User{"Zeeshan", 30, "zeeshan@go.dev", true}
 
 	fmt.Println(userData)
 	userData.GetStatus()
+	userData.UpdateEmail()
+	fmt.Println("Previous email was:", userData.Email)
 }
 
 type User struct {
 	Name       string
 	Age        int
+	Email      string
 	IsLoggedIn bool
 }
 
 func (u User) GetStatus() {
 	fmt.Println("Is User active?", u.IsLoggedIn)
+}
+
+func (u User) UpdateEmail() {
+	u.Email = "zeeshan@acloudtechie.com"
+	fmt.Println("Record has been updated:", u.Email)
 }
