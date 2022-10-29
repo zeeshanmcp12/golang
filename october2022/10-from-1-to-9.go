bb7bead3838fb7fe075fb2148d3652e885b65a90
zeeshanmcp12 <zeeshanmcp12@gmail.com>
2022-10-29 17:53:02 +0500

    add task logic to combine all basic concepts

--- october2022/10-from-1-to-9.go
+++ october2022/10-from-1-to-9.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
 func main() {
 	fmt.Println("Combining all concepts")
+
+	tasks := []string{}
+	fmt.Print("Add task: ")
+
+	for i := 0; true; i++ {
+
+		reader := bufio.NewReader(os.Stdin)
+		input, _ := reader.ReadString('\n')
+		f_txt := strings.TrimSpace(input)
+
+		if f_txt != "done" {
+			if f_txt != "" {
+				tasks = append(tasks, f_txt)
+				fmt.Print("Type 'done' or add task: ")
+			} else {
+				fmt.Print("No input, type 'done' or add task: ")
+			}
+			continue
+		} else if f_txt == "done" {
+			fmt.Println("Thank you for adding tasks: ")
+			for j, val := range tasks {
+				fmt.Printf("Task# %v -> %v \n", j+1, val)
+			}
+			break
+		} else {
+			fmt.Println("Invalid input!")
+		}
+
+	}
 }
