75c98203590c170c17fe34f57b38335c45d221b0
zeeshanmcp12 <zeeshanmcp12@gmail.com>
2022-11-02 12:30:15 +0500

    refactor status and body into functions

--- november2022/12-web-request.go
+++ november2022/12-web-request.go
@@ -15,15 +15,50 @@ import (
 // - Read response whatever we received after requesting web (or website)
 // - ioutil.ReadAll()
 
-const url = "https://acloudtechie.com/about"
+// const url = "https://acloudtechie.com"
+const url = "https://google.com"
 
 func main() {
 	fmt.Println("Handling web request in golang!")
+	// response, err := http.Get(url)
+
+	// checkNilErr(err)
+
+	// fmt.Printf("Type of response is %T\n", response)
+	// defer response.Body.Close()
+
+	// data, err := ioutil.ReadAll(response.Body)
+
+	// content := string(data)
+
+	// fmt.Println(content)
+
+	// Check website status
+	getStatus(url)
+
+	// Print Body of web page
+	getBody(url)
+
+}
+
+func getStatus(url string) {
+	response, err := http.Get(url)
+	checkNilErr(err)
+
+	status := response.Status
+	fmt.Printf("Type of status is %T\n", status)
+
+	fmt.Println("Http status code is: ", status)
+	defer response.Body.Close()
+}
+
+func getBody(url string) {
+
 	response, err := http.Get(url)
 
 	checkNilErr(err)
 
-	fmt.Printf("Type of response is %T", response)
+	fmt.Printf("Type of response is %T\n", response)
 	defer response.Body.Close()
 
 	data, err := ioutil.ReadAll(response.Body)
