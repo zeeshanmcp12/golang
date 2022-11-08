cac3c78b3d50cf42fc925b0183c9968323e624ae
zeeshanmcp12 <zeeshanmcp12@gmail.com>
2022-11-09 00:50:12 +0500

    post request with fake json payload

--- november2022/14-1-webRequest-verbs.go
+++ november2022/14-1-webRequest-verbs.go
@@ -7,11 +7,12 @@ import (
 	"strings"
 )
 
-const siteUrl = "http://localhost:8000/"
+const siteUrl = "http://localhost:8000/post"
 
 func main() {
 	fmt.Println("Web request with Get method!")
-	GetRequest(siteUrl)
+	// GetRequest(siteUrl)
+	PostRequest(siteUrl)
 }
 
 func GetRequest(siteUrl string) {
@@ -32,6 +33,29 @@ func GetRequest(siteUrl string) {
 	responseBuilder.Write(content)
 	fmt.Println(responseBuilder.String())
 
+}
+
+func PostRequest(url string) {
+
+	// Create fake json payload
+	request := strings.NewReader(`
+	{
+		"name":"zeeshan",
+		"age":32,
+		"email":"admin@acloudtechie.com"
+	}
+	`)
+
+	// http -> package
+	// Post -> method that accepts three arguments:
+	// 			url, content format (for example "application/json"), request body or json payload.
+	response, err := http.Post(url, "application/json", request)
+	CheckNilErr(err)
+
+	defer response.Body.Close()
+
+	content, _ := ioutil.ReadAll(response.Body)
+	fmt.Println(string(content))
 }
 
 func CheckNilErr(err error) {
