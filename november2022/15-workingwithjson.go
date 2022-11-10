7b99c012a31b7cf33f5ba3f49e3418ca18fe68b2
zeeshanmcp12 <zeeshanmcp12@gmail.com>
2022-11-10 19:39:35 +0500

    update kitten slice

--- november2022/15-workingwithjson.go
+++ november2022/15-workingwithjson.go
@@ -2,6 +2,12 @@ package main
 
 import "fmt"
 
+// While working with JSON, these are the points
+// Create structure
+// Encode data into json
+// 	To encode data into json we need a function
+// 	Inside this function, we need to create slice and use struct as a slice
+
 type kitten struct {
 	Name  string
 	Age   int
@@ -12,4 +18,12 @@ type kitten struct {
 
 func main() {
 	fmt.Println("Working with JSON!")
+}
+
+func EncodeJson() {
+
+	ourKitten := []kitten{
+		{"Ruby", 60, "Brownish", "Good Hunter", []string{"Fluffy", "Canned Food"}},
+		{"Max", 60, "White", "Keen observer", []string{"Fluffy", "Good food"}},
+	}
 }
