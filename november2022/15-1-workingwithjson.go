a92637de5dbebe03218e3e0dfcb70f3a446a874a
zeeshanmcp12 <zeeshanmcp12@gmail.com>
2022-11-11 18:47:43 +0500

    update json formatting while using backticks etc

---
+++ november2022/15-1-workingwithjson.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Working with Json and it's formatting")
+}
--- november2022/15-workingwithjson.go
+++ november2022/15-workingwithjson.go
@@ -31,11 +31,22 @@ func EncodeJson() {
 		{"Max", 60, "White", "Keen observer", []string{"Fluffy", "Good food"}},
 	}
 
-	// fmt.Println(ourKitten[1])
-	data, err := json.Marshal(ourKitten)
+	// json.Marshal
+	// Marshal -> is a method inside json package.
+	// Marshal -> will not show json data properly (or in json format) and only show data in single line.
+	// data, err := json.Marshal(ourKitten)
+
+	// MarshalIndent -> is like Marshal but applies Indent to format the output. Each JSON element in the output will begin on a new line beginning with prefix followed by one or more copies of indent according to the indentation nesting.
+	data, err := json.MarshalIndent(ourKitten, "", "\t")
+	// json.MarshalIndent(ourKitten, "", "\t")
+	// MarshalIndent -> requires three arguments
+	// 		Interface -> struct (which is ourKitten in our case)
+	// 		Prefix -> any string which will add in the beginning of new line. In our case it is ""(empty string)
+	// 		indent string -> \t -> tab (in our case)
+
 	CheckNilErr(err)
 
-	fmt.Printf("%s\n", string(data))
+	fmt.Printf("%s\n", data)
 }
 
 func CheckNilErr(err error) {
