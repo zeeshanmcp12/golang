b4db482f076988340a86539dc6fb86df9f05be87
zeeshanmcp12 <zeeshanmcp12@gmail.com>
2022-11-14 15:47:23 +0500

    create json schema with values

--- november2022/15-1-workingwithjson.go
+++ november2022/15-1-workingwithjson.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Kitten struct {
+	Name  string   `"json:petName"`
+	Age   int      `"json:age"`
+	Color string   `"json:color"`
+	Food  []string `json:petFood"`
+}
 
 func main() {
 	fmt.Println("Working with Json and it's formatting")
+	EncodeJson()
+}
+
+func EncodeJson() {
+
+	myKitten := []Kitten{
+		{"Ruby", 2, "Brownish", []string{"Fluffy", "Treat"}},
+		{"Max", 2, "White", []string{"Fluffy"}},
+	}
+
+	encodedData, err := json.MarshalIndent(myKitten, "", "\t")
+	CheckNilErr(err)
+
+	fmt.Printf("%s\n", encodedData)
+}
+
+func CheckNilErr(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
