6ec48294b497f269d93cee074a6602d2a473d649
zeeshanmcp12 <zeeshanmcp12@gmail.com>
2022-11-22 13:31:35 +0500

    save and read from file

--- november2022/15-3-workingwithjson.go
+++ november2022/15-3-workingwithjson.go
@@ -3,13 +3,16 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
 )
 
 type User struct {
-	Name  string
-	Age   int
-	Email string
-	Skill []string
+	Name  string   `json:"name"`
+	Age   int      `json:"age"`
+	Email string   `json:"emailAddress"`
+	Skill []string `json:"skills,omitempty"`
 }
 
 func main() {
@@ -22,7 +25,7 @@ func encodeJsonData() {
 
 	userData := []User{
 		{"Abdullah", 30, "abd@go.dev", []string{"Keen observer", "Soft spoken"}},
-		{"Shani", 31, "sha@go.dev", []string{"Good learner", "Soft spoken"}},
+		{"Shani", 31, "sha@go.dev", []string{}},
 	}
 
 	encodedJsonData, err := json.MarshalIndent(userData, "", "\t")
@@ -31,6 +34,23 @@ func encodeJsonData() {
 
 	fmt.Printf("%s", encodedJsonData)
 
+	createFile(string(encodedJsonData))
+
+}
+
+func createFile(data string) {
+	file, err := os.Create("./test2.json")
+	checkNilErr(err)
+
+	io.WriteString(file, data)
+
+	defer file.Close()
+}
+
+func readFile(filename []byte) {
+	fileData, err := ioutil.ReadFile(string(filename))
+
+	checkNilErr(err)
 }
 
 func checkNilErr(err error) {
