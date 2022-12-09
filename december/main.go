3e95d9a274c359ec148e83bb477759f983c48789
zeeshanmcp12 <zeeshanmcp12@gmail.com>
2022-12-10 00:06:53 +0500

    revisiting more concepts

--- december/main.go
+++ december/main.go
@@ -1,13 +1,40 @@
 package main
 
-import "fmt"
+// func main() {
+// 	fmt.Println("Hello World")
 
-func main() {
-	fmt.Println("Hello World")
+// 	var name string = "Sakina"
 
-	var name string = "Sakina"
+// 	for i, j := range name {
+// 		fmt.Printf("%v%c\n", i, j)
+// 	}
+// }
 
-	for i, j := range name {
-		fmt.Printf("%v%c\n", i, j)
-	}
-}
+// func main() {
+// 	var number int
+// 	fmt.Println("Enter any number: ")
+// 	fmt.Scanf("%v", &number)
+
+// 	for i := 1; i < 10+1; i++ {
+// 		fmt.Printf("%v x %v = %v\n", number, i, number*i)
+// 	}
+// }
+
+// func main() {
+// 	fmt.Println("Enter any text: ")
+
+// 	reader := bufio.NewReader(os.Stdin)
+
+// 	input, _ := reader.ReadString('\n')
+// 	fmt.Println("This is what you entered:", input)
+
+// 	fmt.Printf("Type of input is %T\n", input)
+
+// 	strtoInt, err := strconv.Atoi(strings.TrimSpace(input))
+
+// 	fmt.Printf("Now, type of input is %T", strtoInt)
+
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// }
