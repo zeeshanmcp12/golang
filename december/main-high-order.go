c871c712ff8f2c1dc3d39e975440dc7e0fbf39c7
zeeshanmcp12 <zeeshanmcp12@gmail.com>
2022-12-20 19:10:43 +0500

    refactor todo app logic with high order function

--- december/main-high-order.go
+++ december/main-high-order.go
@@ -14,6 +14,36 @@ func main() {
 
 	fmt.Printf("Add tasks: ")
 
+	fmt.Printf("Enter \n 1- Add Task\n 2- Show Task")
+
+	addTask(tasks...)
+
+	// for true {
+	// 	reader := bufio.NewReader(os.Stdin)
+	// 	input, _ := reader.ReadString('\n')
+	// 	f_txt := strings.TrimSpace(input)
+
+	// 	if f_txt != "done" {
+	// 		if f_txt != "" {
+	// 			tasks = append(tasks, f_txt)
+	// 			fmt.Printf("Type done or Press Enter: ")
+	// 		} else {
+	// 			fmt.Printf("Invalid input! Type done or Press Enter: ")
+	// 			continue
+	// 		}
+	// 	} else if f_txt == "done" {
+	// 		fmt.Printf("Thank you for adding tasks:\n")
+	// 		for i, val := range tasks {
+	// 			fmt.Printf("%v -> %v\n", i, val)
+	// 		}
+	// 		break
+	// 	} else {
+	// 		fmt.Printf("Invalid input! Try again.")
+	// 	}
+	// }
+}
+
+func addTask(tasks ...string) {
 	for true {
 		reader := bufio.NewReader(os.Stdin)
 		input, _ := reader.ReadString('\n')
@@ -36,5 +66,11 @@ func main() {
 		} else {
 			fmt.Printf("Invalid input! Try again.")
 		}
+	}
+}
+
+func showTask(tasks ...string) {
+	for i, val := range tasks {
+		fmt.Printf("%v -> %v\n", i, val)
 	}
 }
