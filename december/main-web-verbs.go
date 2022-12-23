c6653e9878bd2c3e8b1fc7de17c73f4c9b418a2b
zeeshanmcp12 <zeeshanmcp12@gmail.com>
2022-12-23 20:16:43 +0500

    revisit web verbs i.e. get post etc

--- december/main-web-verbs.go
+++ december/main-web-verbs.go
@@ -9,7 +9,8 @@ import (
 
 func main() {
 	fmt.Println("Web verbs i.e. get, post etc")
-	PerformGetRequest()
+	// PerformGetRequest()
+	PerformPostRequest()
 
 }
 
@@ -30,6 +31,28 @@ func PerformGetRequest() {
 	stringBody, err := responseBuilder.Write(resBody)
 	fmt.Println("Content length: ", stringBody)
 	fmt.Println(responseBuilder.String())
+
+}
+
+func PerformPostRequest() {
+	const myUrl = "http://localhost:8000/post"
+
+	// fake json payload
+	jsonData := strings.NewReader(`
+		{
+			"author":"zeeshan",
+			"blog":"http://acloudtechie.com"
+		}
+	`)
+
+	response, err := http.Post(myUrl, "application/json", jsonData)
+
+	checkNilErr(err)
+	defer response.Body.Close()
+
+	content, _ := ioutil.ReadAll(response.Body)
+
+	fmt.Println(string(content))
 
 }
 
