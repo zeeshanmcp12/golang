50d1ecc5df3f55433499e5e2b7f8c01b5e5aa66a
zeeshanmcp12 <zeeshanmcp12@gmail.com>
2022-12-24 15:55:35 +0500

    make postform request

--- december/main-web-verbs.go
+++ december/main-web-verbs.go
@@ -4,13 +4,25 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"net/url"
 	"strings"
 )
+
+// Following packages and methods are used to work with web verbs (get/post) etc
+// http
+// 	http.Get
+// 	http.Post
+// ioutil
+// 	ioutil.ReadAll
+// strings
+// 	strings.Builder
+// 	strings.NewReader
 
 func main() {
 	fmt.Println("Web verbs i.e. get, post etc")
 	// PerformGetRequest()
-	PerformPostRequest()
+	// PerformPostRequest()
+	PerformPostFormRequest()
 
 }
 
@@ -49,6 +61,25 @@ func PerformPostRequest() {
 	defer response.Body.Close()
 
 	content, _ := ioutil.ReadAll(response.Body)
+	fmt.Println(string(content))
+
+}
+
+func PerformPostFormRequest() {
+	const myUrl = "http://localhost:8000/postform"
+
+	data := url.Values{}
+	data.Add("firstname", "Muhammad")
+	data.Add("lastname", "Zeeshan")
+	data.Add("blog", "https://acloudtechie.com")
+
+	response, err := http.PostForm(myUrl, data)
+	checkNilErr(err)
+
+	defer response.Body.Close()
+
+	content, _ := ioutil.ReadAll(response.Body)
+
 	fmt.Println(string(content))
 
 }
