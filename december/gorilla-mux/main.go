573e665d4277bad5ea31f04f0c9b49c3621b039a
zeeshanmcp12 <zeeshanmcp12@gmail.com>
2022-12-27 21:06:26 +0500

    add another router called profile

--- december/gorilla-mux/main.go
+++ december/gorilla-mux/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -12,15 +14,36 @@ func main() {
 	greeter()
 	r := mux.NewRouter()
 	r.HandleFunc("/", serveHome).Methods("GET")
+	r.HandleFunc("/profile", profile).Methods("GET")
 
-	http.ListenAndServe(":4000", r)
+	log.Fatal(http.ListenAndServe(":4000", r))
 }
 
 func greeter() {
-	fmt.Println("Hi, Server is listening on http://localhost:4000")
+	fmt.Println("Server is listening on http://localhost:4000")
 }
 
 func serveHome(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("<h1>Welcome to https://acloudtechie.com</h1>"))
 
+}
+
+func profile(w http.ResponseWriter, r *http.Request) {
+	jsonData := []Profile{
+		{"Zeeshan", 32, "acloudtechie.com"},
+		{"Abdullah", 30, "abc.com"},
+	}
+
+	finalJSON, _ := json.MarshalIndent(jsonData, "", "\t")
+
+	fmt.Println(string(finalJSON))
+
+	w.Write([]byte(finalJSON))
+
+}
+
+type Profile struct {
+	Name    string
+	Age     int
+	Website string
 }
