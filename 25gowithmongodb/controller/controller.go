1cbea9b619336f3205210222fbe4d768b97b1a40
zeeshanmcp12 <zeeshanmcp12@gmail.com>
2023-01-09 11:21:44 +0500

    update controller code

--- 25gowithmongodb/controller/controller.go
+++ 25gowithmongodb/controller/controller.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"model"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
