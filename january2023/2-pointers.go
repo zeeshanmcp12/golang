2f6a0610f22d3ebeb4f6828ba2bb8aff6cff33cd
zeeshanmcp12 <zeeshanmcp12@gmail.com>
2023-01-09 12:10:30 +0500

    more on pointers in january2023

--- january2023/2-pointers.go
+++ january2023/2-pointers.go
@@ -4,7 +4,6 @@ import "fmt"
 
 func main() {
 	fmt.Println("Working with Pointers in golang!")
-	fmt.Println()
 	fmt.Println("--------------------------------------")
 
 	var i, j = 42, 2701
@@ -18,6 +17,16 @@ func main() {
 	var p = &i
 	fmt.Println("New variable p -> (var p = &i)")
 
-	fmt.Printf("Value of p -> %v is the address of i and value of *p is the value at that address -> %v", p, *p)
+	fmt.Printf("Value of p -> %v is the address of i and value of *p is the value at that address -> %v\n", p, *p)
+	fmt.Println("--------------------------------------")
+	fmt.Println("Let's change the value of i by assigning new value to *p -> *p = 21. It should change the value of i to 21")
+
+	*p = 21
+	fmt.Printf("Value of *p -> %v\n", *p)
+	fmt.Printf("Value of i -> %v, hence the value of i has changed \n", i)
+
+	fmt.Println("--------------------------------------")
+	fmt.Println("--------------------------------------")
+	fmt.Println("Let's change the value of ")
 
 }
