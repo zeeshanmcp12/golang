976166d9ee4987804cef1b7635755c632e5d7e23
zeeshanmcp12 <zeeshanmcp12@gmail.com>
2023-01-14 19:07:01 +0500

    some common logic concepts

--- january2023/3-main.go
+++ january2023/3-main.go
@@ -11,21 +11,32 @@ func main() {
 	fmt.Println("Todo App!")
 
 	tasks := []string{}
+	fmt.Printf("Add tasks: ")
 
 	for true {
-
-		fmt.Println("Add tasks: ")
 
 		reader := bufio.NewReader(os.Stdin)
 		input, _ := reader.ReadString('\n')
 		f_text := strings.TrimSpace(input)
 
-		if f_text != "" {
-
+		if f_text != "done" {
+			if f_text != "" {
+				tasks = append(tasks, f_text)
+				fmt.Printf("Enter task or type done: ")
+			} else {
+				fmt.Printf("Text cannot be empty, enter task or type done: ")
+			}
+			continue
+		} else if f_text == "done" {
+			fmt.Println("Here are your tasks: ")
+			for i, val := range tasks {
+				fmt.Printf("Task %v: %v\n", i+1, val)
+			}
+		} else {
+			fmt.Println("Invalid Input. Try Again!")
 		}
-
+		break
 	}
-
 }
 
 func checkNilErr(err error) {
