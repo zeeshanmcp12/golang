3ef6c627dbfa4e5d5b92c8c476831c1615524472
zeeshanmcp12 <zeeshanmcp12@gmail.com>
2023-02-20 16:38:22 +0500

    method lab question with explanation

--- february2023/0.1.0-method-lab-questions.go
+++ february2023/0.1.0-method-lab-questions.go
@@ -13,15 +13,38 @@ func (r Rectangle) area() int {
 
 func (r *Rectangle) incLength(n int) {
 	for i := 0; i < n; i++ {
-		r.length += i
+		// r.length += i
+		r.length = r.length + i
+		// Explanation below when n = 7
+		/*
+			0 -> 5
+			1 -> 6
+			2 -> 8
+			3 -> 11
+			4 -> 15
+			5 -> 20
+			6 -> 26
+			7 -> this time loop will end because 7 is not less than 7 but equal
+		*/
+		// Explanation below when n = 6
+		/*
+			for i := 0; i < 6; i++ { r.length (5) = 5 + 0 }
+			for i := 1; i < 6; i++ { r.length (6) = 5 + 1 }
+			for i := 2; i < 6; i++ { r.length (8) = 6 + 2 }
+			for i := 3; i < 6; i++ { r.length (11) = 8 + 3 }
+			for i := 4; i < 6; i++ { r.length (15) = 11 + 4 }
+			for i := 5; i < 6; i++ { r.length (20) = 15 + 5 }
+			for i := 6; i < 6; i++ { Loop end because 6 is not less than 6 but equal }
+		*/
+		// fmt.Println(r.length)
 	}
 }
 
 func main() {
 	r := Rectangle{breadth: 10, length: 5}
 	fmt.Println(r.area())
-	fmt.Println(r)
-	r.incLength(7)
+	fmt.Printf("%+v\n", r)
+	r.incLength(6)
 	fmt.Println(r.area())
-	fmt.Println(r)
+	fmt.Printf("%+v", r)
 }
