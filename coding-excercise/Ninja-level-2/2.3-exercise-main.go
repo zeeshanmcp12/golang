37a4d33b3b0e3148625173ec18f8ae87f5feabee
zeeshanmcp12 <zeeshanmcp12@gmail.com>
2023-03-11 07:04:37 +0500

    add exercise 3 that contain TYPES and UNTYPED constant

---
+++ coding-excercise/Ninja-level-2/2.3-exercise-main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+/*
+Create TYPED and UNTYPED constants. Print the values of the constants.
+
+*/
+
+const username string = "Zeeshan"
+const emailAddress = "abd@go.dev"
+
+func main() {
+	fmt.Println("Create TYPED and UNTYPED constants")
+
+	fmt.Println(username, emailAddress)
+}
