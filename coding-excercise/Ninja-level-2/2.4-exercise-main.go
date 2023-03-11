3a738a165a7dbd77fa47a2d5e20984afdc73ef16
zeeshanmcp12 <zeeshanmcp12@gmail.com>
2023-03-11 12:13:45 +0500

    add exercise 4 with step one

---
+++ coding-excercise/Ninja-level-2/2.4-exercise-main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+/*
+Write a program that
+● assigns an int to a variable
+● prints that int in decimal, binary, and hex
+● shifts the bits of that int over 1 position to the left, and assigns that to a variable
+● prints that variable in decimal, binary, and hex
+
+*/
+
+var intNumber int
+
+func main() {
+	fmt.Println("Exercise 4 in Ninja level 2")
+
+	intNumber = 42
+
+	fmt.Printf("Binary -> %b\t Decimal -> %d\t Hex -> %#x", intNumber, intNumber, intNumber)
+}
