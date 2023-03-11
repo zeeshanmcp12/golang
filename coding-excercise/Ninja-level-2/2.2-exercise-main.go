982170ffe0a0dd8c9c37cb8843a47d6bc017e02f
zeeshanmcp12 <zeeshanmcp12@gmail.com>
2023-03-11 06:39:01 +0500

    write some expressions and assign their values to var

--- coding-excercise/Ninja-level-2/2.2-exercise-main.go
+++ coding-excercise/Ninja-level-2/2.2-exercise-main.go
@@ -25,18 +25,29 @@ var (
 func main() {
 	fmt.Println("Exercise 2 - Ninja level 2")
 
-	var num1 int = 20
-	var num2 int = 25
+	// var num1 int = 20
+	// var num2 int = 25
 
-	equality = num1 == num2
-	lessThanEqual = num1 <= num2
-	greaterThanEqual = num1 >= num2
-	notEquality = num1 != num2
-	lessThan = num1 < num2
-	greaterThan = num1 > num2
+	// equality = num1 == num2
+	// lessThanEqual = num1 <= num2
+	// greaterThanEqual = num1 >= num2
+	// notEquality = num1 != num2
+	// lessThan = num1 < num2
+	// greaterThan = num1 > num2
 
-	fmt.Printf("%d == %d -> %t\n%d <= %d -> %t\n%d >= %d -> %t\n%d != %d ->%t\n%d < %d ->%t\n%d > %d ->%t\n", num1, num2, equality, num1, num2, lessThanEqual, num1, num2, greaterThanEqual, num1, num2, notEquality, num1, num2, lessThan, num1, num2, greaterThan)
+	// fmt.Printf("%d == %d -> %t\n%d <= %d -> %t\n%d >= %d -> %t\n%d != %d ->%t\n%d < %d ->%t\n%d > %d ->%t\n", num1, num2, equality, num1, num2, lessThanEqual, num1, num2, greaterThanEqual, num1, num2, notEquality, num1, num2, lessThan, num1, num2, greaterThan)
 
 	// Above is not the efficient code as it has so much copy pasting
+
+	// Simple solution for the problem mentioned at line 6
+
+	a := 42 == 43
+	b := 42 <= 43
+	c := 42 >= 43
+	d := 42 != 43
+	e := 42 < 43
+	f := 42 > 43
+
+	fmt.Println(a, b, c, d, e, f)
 
 }
