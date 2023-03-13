e84286847169a976202f4bd17caae5fc39e4e63e
zeeshanmcp12 <zeeshanmcp12@gmail.com>
2023-03-13 18:46:24 +0500

    bitwise or example

---
+++ march2023/bitwise.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Bitwise Operators")
+	var num1, num2 = 16, 22
+
+	// bitwise AND
+	// result := num1 & num2
+
+	// bitwise OR
+	result := num1 | num2
+	fmt.Printf("%b\n%b\n", num1, num2)
+	fmt.Println(result)
+}
+
+/*
+
+0011 &
+0101
+0001 -> 1
+
+01100
+10110
+00100 -> 4
+
+10000
+10110
+10000 -> 16
+*/
+
+/*
+10000
+10110
+10110 -> 22
+
+
+*/
